ff24ff709385aa75f89d8dadc217b54bc007faa6
danielfransa <daniel_fransa@hotmail.com>
2023-11-15 14:00:41 -0300

    Ajuste hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Permitir todos os hosts para o ambiente de produção
+  config.hosts.clear
 end
