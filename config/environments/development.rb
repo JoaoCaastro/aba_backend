5b2265b69ac0acaa73fec45023a2389cf0868165
danielfransa <daniel_fransa@hotmail.com>
2023-11-16 21:38:28 -0300

    ajustar rota

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,5 +62,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "railsapi1-jt6xsoyo.b4a.run/" 
+  config.hosts << "railsapi1-jt6xsoyo.b4a.run"
 end
